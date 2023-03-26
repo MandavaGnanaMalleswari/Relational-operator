3e3c7a723fcd51ed26a3b9683dc9a3afcae376fc
MandavaGnanaMalleswari <128772693+MandavaGnanaMalleswari@users.noreply.github.com>
2023-03-26 21:22:29 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,35 @@
+class relational
+{
+    public static void main(String args[])
+    {
+        //relational operators
+        int x = 6;
+        int y = 9;
+        double a = 7.8;
+        double b = 9.8;
+        boolean q = x < y;
+        boolean w = x > y;
+        boolean e = a < b;
+        boolean r = a > b;
+        boolean t = a >= b;
+        boolean u = a <= b;
+        boolean i = a == b;
+        boolean o = a != b;
+        boolean p = x >= y;
+        boolean s = x <= y;
+        boolean d = x == y;
+        boolean f = x != y;
+        System.out.println(q);
+        System.out.println(w);
+        System.out.println(e);
+        System.out.println(r);
+        System.out.println(t);
+        System.out.println(u);
+        System.out.println(i);
+        System.out.println(o);
+        System.out.println(p);
+        System.out.println(s);
+        System.out.println(d);
+        System.out.println(f);
+    }
+}
